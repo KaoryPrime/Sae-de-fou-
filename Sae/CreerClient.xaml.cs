1a993dc9d7b505940b6ea2a50a27c9f9502b4bf0
sailletn <natis.saillet@etu.univ-smb.fr>
2025-06-06 11:11:35 +0200

    test

---
+++ Sae/CreerClient.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sae
+{
+    /// <summary>
+    /// Logique d'interaction pour CreerClient.xaml
+    /// </summary>
+    public partial class CreerClient : Page
+    {
+        public CreerClient()
+        {
+            InitializeComponent();
+        }
+    }
+}
